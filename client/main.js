983b5fe3a6008b33867bcfe81024115ce732644a
Cosima Peter <cs.manughian@gmail.com>
2020-03-07 22:05:22 -0800

    Append to root div

--- client/main.js
+++ client/main.js
@@ -4,15 +4,18 @@ import { socket } from './modules/socket_client.js';
 
 (function () {
 
-    const ws = socket.init();
+    let root = document.getElementById('root');
 
-    console.log(ws)
+    const ws = socket.init();
 
     ws.onmessage = function(event) {
         const data = JSON.parse(event.data);
         console.log('Got reply: ', data);
 
-        document.body.append(
+        // Erase previous render
+        // root.innerHTML = '';
+
+        root.appendChild(
             graph.init(data)
         );
     };
--- client/modules/graph.js
+++ client/modules/graph.js
@@ -5,18 +5,18 @@ export var graph = (function () {
 
             // const data = [4, 8, 15, 16, 23, 42];
 
-            const div = d3.create("div")
-                .style("font", "10px sans-serif")
-                .style("text-align", "right")
-                .style("color", "white");
+            const div = d3.create('div')
+                .style('font', '10px sans-serif')
+                .style('text-align', 'right')
+                .style('color', 'white');
       
-            div.selectAll("div")
+            div.selectAll('div')
                 .data(data)
-                .join("div")
-                    .style("background", "steelblue")
-                    .style("padding", "3px")
-                    .style("margin", "1px")
-                    .style("width", d => `${d * 10}px`)
+                .join('div')
+                    .style('background', 'steelblue')
+                    .style('padding', '3px')
+                    .style('margin', '1px')
+                    .style('width', d => `${d * 10}px`)
                     .text(d => d);
         
             return div.node();
