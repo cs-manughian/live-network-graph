625402584dd01f44f02bcb558826166b08d23472
Cosima Peter <cs.manughian@gmail.com>
2020-03-08 12:27:43 -0700

    Playing with hierarchical edge bundling graph

--- client/main.js
+++ client/main.js
@@ -4,20 +4,28 @@ import { socket } from './modules/socket_client.js';
 
 (function () {
 
+    /*
+     * Initialize the graph with empty data
+     */
     let root = document.getElementById('root');
+    // root.appendChild(graph.init([]));
 
+    root.append(graph.bundleEdges())
+
+    /*
+     * Initialize socket connection and message event handler
+     */
     const ws = socket.init();
 
     ws.onmessage = function(event) {
+
         const data = JSON.parse(event.data);
         console.log('Got reply: ', data);
 
         // Erase previous render
         // root.innerHTML = '';
 
-        root.appendChild(
-            graph.init(data)
-        );
+        root.appendChild(graph.init(data));
 
         // TODO: 
         // Trigger update graph functions based on reply without 
